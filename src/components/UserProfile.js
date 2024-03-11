aac95c1c481fd514deca0ecdb05e9be446812ddc
Nipuna Rambukkanage <nipunaram@gmail.com>
2024-03-11 21:01:05 +0530

    Added getting user profile from backend

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar, Card, CardContent, Grid, Typography, Paper } from '@mui/material';
 import { PaidOutlined } from '@mui/icons-material';
+import axios from 'axios';
 
 const UserProfile = ()  => {
+    const [user, setUser] = useState(null);
 
-    const user = {
-        name : 'Tharindu Nanayakkara',
-        school : 'Central College, Colombo',
-        dob : 'January 3rd, 2009',
-        email : 'tharindu@gmail.com',
-        description : 'I am a programming enthusiast!',
-        hobby : '',
-        hometown : '',
-        age : '',
-        phone : '0112222222',
-        ambition : ''
-    };
+    useEffect(() => {
+        const fetchUserData = async ()=>{
+            try{
+                const response = await axios.get('http://localhost:5000/get_user');
+                setUser(response?.data);
+            }catch{
+                console.error("Error fetching user data : ", error);
+            }
+        };
+
+    fetchUserData();
+    }, []);
+
+    // const user = {
+    //     name : 'Tharindu Nanayakkara',
+    //     school : 'Central College, Colombo',
+    //     dob : 'January 3rd, 2009',
+    //     email : 'tharindu@gmail.com',
+    //     description : 'I am a programming enthusiast!',
+    //     hobby : '',
+    //     hometown : '',
+    //     age : '',
+    //     phone : '0112222222',
+    //     ambition : ''
+    // };
+
+
 
     return (
        <Grid container spacing={3}>
@@ -33,16 +50,19 @@ const UserProfile = ()  => {
         <Grid item xs={12} md={8} lg={9} style={{ marginTop : "64px"}}>
             <Paper style={{ padding : 20 }}>
                 <Typography variant="subtitle1">
-                    <strong>School : </strong> {user.school}
+                    <strong>Name : </strong> {user?.name}
                 </Typography>
                 <Typography variant="subtitle1">
-                    <strong>Date of Birth : </strong> {user.dob}
+                    <strong>School : </strong> {user?.school}
                 </Typography>
                 <Typography variant="subtitle1">
-                    <strong>Email : </strong> {user.email}
+                    <strong>Date of Birth : </strong> {user?.dob}
                 </Typography>
                 <Typography variant="subtitle1">
-                    <strong>Description : </strong> {user.description}
+                    <strong>Email : </strong> {user?.email}
+                </Typography>
+                <Typography variant="subtitle1">
+                    <strong>Description : </strong> {user?.description}
                 </Typography>
             </Paper>
         </Grid>
